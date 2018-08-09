79410b73165338208e4dbd80d13c0eba6b914472
LXF <1542147783@qq.com>
2018-08-10 01:28:00 +0800

    java-code--jni初步使用

--- lxf-base/src/test/java/jvm/Test2.java
+++ lxf-base/src/test/java/jvm/Test2.java
@@ -1,13 +1,36 @@
 package jvm;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.sun.xml.internal.fastinfoset.util.ValueArray.MAXIMUM_CAPACITY;
+
 public class Test2 {
     private String s = "0";
+
+    static final int tableSizeFor(int cap) {
+        int n = cap - 1;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;
+        n |= n >>> 8;
+        n |= n >>> 16;
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+    }
 
     public String getS() {
         return s;
     }
 
     public void setS(String s) {
+
         this.s = s;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(tableSizeFor(1));
+        Map map = new HashMap(1);
+        map.put("1", "e");
     }
 }
